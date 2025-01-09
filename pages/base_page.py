8e3a01f6d316e1c7c689d7f7ec20c807d2b96144
Anna <anna.nikolenko@lumiprobe.com>
2025-01-09 13:25:55 +0700

    Base page file is added

---
+++ pages/base_page.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def find_element(self, locator):
+        return WebDriverWait(self.driver, timeout=40).until(EC.element_to_be_clickable(locator),
+                                                            message=f"Element is NOT CLICKABLE{locator}")
+
+    def open_site(self, url):
+        self.driver.get(url)
+
+    def click(self, homepage):
+        return self.find_element(homepage).click()
