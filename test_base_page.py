52fe700e5c89afa0822bcc46b2727ce5cfaa8a37
Anna <anna.nikolenko@lumiprobe.com>
2025-01-19 23:40:27 +0700

    Try to fix again

--- test_base_page.py
+++ test_base_page.py
@@ -1,5 +1,4 @@
 import allure
-import pytest
 from locators.base_page_locators import URL, HOMEPAGE, SINGLE_ELEMENTS, INPUTS, COPYRIGHT_URL
 from pages.base_page import BasePage
 
@@ -13,19 +12,9 @@ def test_first(browser):
     assert browser.current_url == URL
 
 
-@pytest.mark.smoke
 @allure.title("Клик по кнопке Single Elements")
 def test_second(browser):
     base_page = BasePage(browser)
     base_page.open_site(URL)
     base_page.click(SINGLE_ELEMENTS)
     assert INPUTS
-
-
-@pytest.mark.skip
-@allure.title("Проверка, что отображается ссылка в футере")
-def test_copyright_url_is_shown(browser):
-    base_page = BasePage(browser)
-    base_page.open_site(URL)
-    base_page.click(SINGLE_ELEMENTS)
-    assert base_page.get(COPYRIGHT_URL)
